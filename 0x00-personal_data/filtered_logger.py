8ef3682dc331dcd294e617dc669b43165171f8c7
Erickkaranja <karanja99erick@gmail.com>
2023-11-02 09:16:35 +0300

    updating filter_logger file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,57 @@
 #!/usr/bin/env python3
 '''implementing a filter_logger module.'''
+import os
 import re
-from typing import List
+from typing import List, Tuple, Dict
+import logging
+import mysql.connector
 
-
-def filter_datum(fields: List[str], redaction: str, message: List[str],
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    parts = message.split(separator)
     for field in fields:
-        pattern = re.compile(rf'{field}=[^;]+')
-        message = pattern.sub(f'{field}={redaction}', message)
+        message = re.sub('{}=[^{}]+'.format(field, separator),
+                         '{}={}'.format(field, redaction), message)
     return message
+
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields=None):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields: str = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg: str = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
+
+PII_FIELDS: Tuple[str] = ("name", "email", "phone", "ssn", "password")
+
+def get_logger() -> logging.Logger:
+    '''creates a logging.Logger object.'''
+    logger = login.getlogger('user_data')
+    logger.setLevel(logging.INFO)
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+def get_db():
+    ''''''
+    config: Dict = {
+        'user': os.getenv("PERSONAL_DATA_DB_USERNAME", 'root'),
+        'password': os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
+        'host': os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
+        'database': os.getenv("PERSONAL_DATA_DB_NAME"),
+    }
+
+    return mysql.connector.connect(**config)
