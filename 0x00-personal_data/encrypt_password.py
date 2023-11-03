340815e4dfc36b014d54557a85b7221f1f481b62
Erickkaranja <karanja99erick@gmail.com>
2023-11-03 19:21:05 +0300

    updating type annotations

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,14 +3,13 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     '''implementing password hashing using bcrypt'''
-    bytes: byte = password.encode('utf-8')
-    salt: str = bcrypt.gensalt()
-    hash: byte = bcrypt.hashpw(bytes, salt)
-    return hash
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
 
 
-def is_valid(hashed_password: byte, password: str) -> bool:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     '''checks a password against a hashed value.'''
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
