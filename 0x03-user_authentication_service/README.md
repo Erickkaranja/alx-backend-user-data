f2936046f8b9a84dc0d85c9cc774da0046b61653
Erickkaranja <karanja99erick@gmail.com>
2023-11-17 14:38:44 +0300

    initial commit

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,3 @@
+ User authentication service
+The flask app should only interact with Auth and never with DB directly.
+Only public methods of Auth and DB should be used outside these classes
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""creating a sqlalchemy model user."""
+
+from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class User(Base):
+    """initializing class user."""
+    __tablename__: str = 'users'
+
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: str = Column(String(250))
+    reset_token: str = Column(String(250))
